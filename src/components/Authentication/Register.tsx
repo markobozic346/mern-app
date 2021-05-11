74945cae8fc9c9a67ee2591e2c72023bd3e83b4b
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-05-11 09:03:00 +0200

    added basic style for register component

--- src/components/Authentication/Register.tsx
+++ src/components/Authentication/Register.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './Register.scss'
 interface Props {
 
 }
@@ -7,10 +7,22 @@ interface Props {
 const Register = (props: Props) => {
     return (
         <div className='register-container'>
-            <form>
-                <label htmlFor="username">username</label>
-                <input type="text" name='username' placeholder='enter your username here' />
 
+            <form>
+                <h1>Register</h1>
+                <label>username:
+                    <input type="text" name='username' placeholder='enter your username here' />
+                </label>
+                <label>email:
+                    <input type="mail" name='email' placeholder='enter your mail here' />
+                </label>
+                <label>password:
+                    <input type="password" name='password' placeholder='enter password' />
+                </label>
+                <label>confirm password:
+                    <input type="password" name='confirm-password' placeholder='enter password' />
+                </label>
+                <input className='btn-register' type="submit" value='Register' />
             </form>
         </div>
     )
