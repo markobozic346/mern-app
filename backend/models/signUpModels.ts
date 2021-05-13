7f1f0fd8f81f54390c930b6e0df2861b60836b8e
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-05-13 08:35:09 +0200

    added mongoose signup schema

---
+++ backend/models/signUpModels.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const signupTemplate = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const mongooseModel = mongoose.model("usertable", signupTemplate);
+
+export default mongooseModel;
