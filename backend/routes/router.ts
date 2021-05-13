5ce9be73efe82e6461969d9872535ae8d4c17b43
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-05-13 08:53:28 +0200

    added signup router logic

--- backend/routes/router.ts
+++ backend/routes/router.ts
@@ -1,9 +1,29 @@
 import express from "express";
+import mongooseModel from "../models/signUpModels";
 
 const router = express.Router();
 
+type UserData = {
+  email: string;
+  username: string;
+  password: string;
+};
+
 router.post("/signup", (req, res) => {
-  res.send("send");
+  const signupUser = new mongooseModel({
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  signupUser
+    .save()
+    .then((data: UserData) => {
+      res.json(data);
+    })
+    .catch((error: string) => {
+      res.json(error);
+    });
 });
 
 export default router;
