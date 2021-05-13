ffcb94066f1d5fb007c30bd49f9a64fab4ceb021
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-05-13 16:16:20 +0200

    connected database

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Register from './components/Authentication/Register'
 import './App.scss';
 import Login from './components/Authentication/Login';
+import Feed from './components/Feed/Feed';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+
+      <Feed />
     </div>
   );
 }
---
+++ src/components/Feed/Feed.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const Feed = (props: Props) => {
+    return (
+        <div className='feed'>
+            <h1>You must be login to see this page.</h1>
+        </div>
+    )
+}
+
+export default Feed
