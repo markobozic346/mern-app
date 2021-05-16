47c83673513fe1ba3c2ee70f86a63e684cafc799
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-05-16 09:01:25 +0200

    refactored registration userdata state

--- src/components/Authentication/Register.tsx
+++ src/components/Authentication/Register.tsx
@@ -13,37 +13,26 @@ const Register = (props: Props) => {
 
     })
 
-    const handleChangeUsername = (value: string) => {
+    const handleUserDataChange = (value: string, property: string) => {
         setUserData({
             ...userData,
-            username: value,
+            [property]: value,
         })
     }
-    const handleChangeEmail = (value: string) => {
-        setUserData({
-            ...userData,
-            email: value,
-        })
-    }
-    const handleChangePassword = (value: string) => {
-        setUserData({
-            ...userData,
-            password: value,
-        })
-    }
+
     return (
         <div className='register-container'>
 
             <form>
                 <h1>Register</h1>
                 <label>username:
-                    <input onChange={(e) => { handleChangeUsername(e.currentTarget.value) }} type="text" name='username' placeholder='enter your username here' />
+                    <input onChange={(e) => { handleUserDataChange(e.currentTarget.value, "username") }} value={userData.username} type="text" name='username' placeholder='enter your username here' />
                 </label>
                 <label>email:
-                    <input onChange={(e) => { handleChangeEmail(e.currentTarget.value) }} type="mail" name='email' placeholder='enter your mail here' />
+                    <input onChange={(e) => { handleUserDataChange(e.currentTarget.value, "email") }} value={userData.email} type="mail" name='email' placeholder='enter your mail here' />
                 </label>
                 <label>password:
-                    <input onChange={(e) => { handleChangePassword(e.currentTarget.value) }} type="password" name='password' placeholder='enter password' />
+                    <input onChange={(e) => { handleUserDataChange(e.currentTarget.value, "password") }} value={userData.password} type="password" name='password' placeholder='enter password' />
                 </label>
 
                 <input className='btn-submit' type="submit" value='Register' />
