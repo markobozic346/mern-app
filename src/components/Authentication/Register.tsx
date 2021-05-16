c7e4ab509e353c9c8e235936e42ebdb754634405
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-05-16 08:30:03 +0200

    added userdata state for register form

--- src/components/Authentication/Register.tsx
+++ src/components/Authentication/Register.tsx
@@ -1,27 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Register.scss'
+
 interface Props {
 
 }
 
 const Register = (props: Props) => {
+    const [userData, setUserData] = useState({
+        username: "",
+        email: "",
+        password: "",
+
+    })
+
+    const handleChangeUsername = (value: string) => {
+        setUserData({
+            ...userData,
+            username: value,
+        })
+    }
+    const handleChangeEmail = (value: string) => {
+        setUserData({
+            ...userData,
+            email: value,
+        })
+    }
+    const handleChangePassword = (value: string) => {
+        setUserData({
+            ...userData,
+            password: value,
+        })
+    }
     return (
         <div className='register-container'>
 
             <form>
                 <h1>Register</h1>
                 <label>username:
-                    <input type="text" name='username' placeholder='enter your username here' />
+                    <input onChange={(e) => { handleChangeUsername(e.currentTarget.value) }} type="text" name='username' placeholder='enter your username here' />
                 </label>
                 <label>email:
-                    <input type="mail" name='email' placeholder='enter your mail here' />
+                    <input onChange={(e) => { handleChangeEmail(e.currentTarget.value) }} type="mail" name='email' placeholder='enter your mail here' />
                 </label>
                 <label>password:
-                    <input type="password" name='password' placeholder='enter password' />
+                    <input onChange={(e) => { handleChangePassword(e.currentTarget.value) }} type="password" name='password' placeholder='enter password' />
                 </label>
-                <label>confirm password:
-                    <input type="password" name='confirm-password' placeholder='enter password' />
-                </label>
+
                 <input className='btn-submit' type="submit" value='Register' />
             </form>
         </div>
