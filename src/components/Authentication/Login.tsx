18522075cbf15244960c751973fccb3759afa68a
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-05-16 09:40:56 +0200

    added login data state

--- src/components/Authentication/Login.tsx
+++ src/components/Authentication/Login.tsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { validateLocaleAndSetLanguage } from 'typescript';
 import './Login.scss';
 interface Props {
 
 }
 
 const Login = (props: Props) => {
+    const [loginData, setLoginData] = useState({
+        username: '',
+        password: '',
+    })
+
+    const handleLoginDataChange = (value: string, property: string) => {
+        setLoginData({
+            ...loginData,
+            [property]: value,
+        })
+    }
     return (
         <div className='login-container'>
             <form>
                 <h1>Login</h1>
                 <label>
                     Username:
-                    <input type="text" placeholder='enter username here' />
+                    <input onChange={(e) => { handleLoginDataChange(e.target.value, "username") }} type="text" placeholder='enter username here' />
                 </label>
                 <label>
                     Password:
-                    <input type='password' placeholder='enter password here' />
+                    <input onChange={(e) => { handleLoginDataChange(e.target.value, "password") }} type='password' placeholder='enter password here' />
                 </label>
 
                 <input className='btn-submit' type='submit' value='Login' />
